c3f46b7c56cc19cc73c028f95765c25826d96647
Marcos Cereijo <marcos.rodriguez@pexip.com>
2023-09-05 09:36:58 +0200

    chore: display leave button for the listener

--- src/components/LanguagePanel/LanguagePanel.tsx
+++ src/components/LanguagePanel/LanguagePanel.tsx
@@ -28,25 +28,27 @@ export const LanguageIndicator = (props: LanguageIndicatorProps): JSX.Element =>
       <Text className='LanguageText'>
         {capitalizeFirstLetter(props.languageName)}
       </Text>
-      {props.role === Role.Interpreter &&
       <div className='Toolbar'>
-        <Tooltip text={audioMuted ? 'Unmute microphone' : 'Mute microphone'} position='bottom'>
-          <Button modifier='square' onClick={() => { handleMuteAudio().catch((e) => { console.error(e) }) }} variant={audioMuted ? 'danger' : 'primary'}>
-            <Icon source={audioMuted ? IconTypes.IconMicrophoneOff : IconTypes.IconMicrophoneOn} />
-          </Button>
-        </Tooltip>
-        <Tooltip text='Change microphone' position='bottom'>
-          <Button modifier='square' onClick={() => { showSelectMicForm().catch((e) => { console.error(e) }) }} >
-            <Icon source={IconTypes.IconSettings} />
-          </Button>
-        </Tooltip>
+        {props.role === Role.Interpreter &&
+          <>
+            <Tooltip text={audioMuted ? 'Unmute microphone' : 'Mute microphone'} position='bottom'>
+              <Button modifier='square' onClick={() => { handleMuteAudio().catch((e) => { console.error(e) }) }} variant={audioMuted ? 'danger' : 'primary'}>
+                <Icon source={audioMuted ? IconTypes.IconMicrophoneOff : IconTypes.IconMicrophoneOn} />
+              </Button>
+            </Tooltip>
+            <Tooltip text='Change microphone' position='bottom'>
+              <Button modifier='square' onClick={() => { showSelectMicForm().catch((e) => { console.error(e) }) }} >
+                <Icon source={IconTypes.IconSettings} />
+              </Button>
+            </Tooltip>
+          </>
+        }
         <Tooltip text='Leave interpretation' position='bottom'>
           <Button modifier='square' onClick={() => { showDisconnectPrompt().catch((e) => { console.error(e) }) }} aria-label='Test'>
             <Icon source={IconTypes.IconLeave} />
           </Button>
         </Tooltip>
       </div>
-      }
     </Box>
   )
 }
