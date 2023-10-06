3127e857b5d8c264f1c1a06c8e0209a4944cb52c
Marcos Cereijo <marcos.rodriguez@pexip.com>
2023-10-06 14:17:29 +0200

    feat: iframe draggable

--- src/components/LanguagePanel/LanguagePanel.tsx
+++ src/components/LanguagePanel/LanguagePanel.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-import { Box, Button, Icon, IconTypes, Text, Tooltip } from '@pexip/components'
+import { Box, BoxHeader, Button, Icon, IconTypes, RangeSlider, Text, Tooltip } from '@pexip/components'
 import { capitalizeFirstLetter } from '../../utils'
 import { Role } from '../../role'
 import { Interpretation } from '../../interpretation'
@@ -8,6 +8,7 @@ import { showDisconnectPrompt } from '../../prompts'
 
 import './LanguagePanel.scss'
 import { showSelectMicForm } from '../../forms'
+import { moveIFrame, toggleIFramePointerEvents, togglePointerEvents } from '../../iframe'
 
 interface LanguageIndicatorProps {
   languageName: string
@@ -16,38 +17,81 @@ interface LanguageIndicatorProps {
 
 export const LanguageIndicator = (props: LanguageIndicatorProps): JSX.Element => {
   const [audioMuted, setAudioMuted] = useState(false)
+  const [volume, setVolume] = useState<number>(0)
+
+  const refDragButton = useRef<HTMLButtonElement>(null)
 
   const handleMuteAudio = async (): Promise<void> => {
     await Interpretation.setAudioMuted(!audioMuted)
     setAudioMuted(!audioMuted)
   }
 
+  useEffect(() => {
+    let dragging = false
+
+    const handleMove = (event: MouseEvent): void => {
+      if (dragging) {
+        const x = event.pageX
+        const y = event.pageY
+        moveIFrame(x, y)
+      }
+    }
+
+    if (refDragButton?.current != null) {
+      refDragButton.current.addEventListener('mousedown', (e) => {
+        dragging = true
+        toggleIFramePointerEvents(false)
+        e.preventDefault()
+        parent.document.body.addEventListener('mouseup', () => {
+          dragging = false
+          toggleIFramePointerEvents(true)
+        }, { once: true })
+      })
+      parent.document.addEventListener('mousemove', handleMove)
+    }
+    return () => {
+      parent.document.removeEventListener('mousemove', handleMove)
+    }
+  }, [refDragButton])
+
   return (
-    <Box className='LanguageIndicator'>
-      <Icon source={IconTypes.IconSupport} className='LanguageIcon' color=''/>
-      <Text className='LanguageText'>
-        {capitalizeFirstLetter(props.languageName)}
-      </Text>
-      <div className='Toolbar'>
-        {props.role === Role.Interpreter &&
-          <>
-            <Tooltip text={audioMuted ? 'Unmute microphone' : 'Mute microphone'} position='bottom'>
-              <Button modifier='square' onClick={() => { handleMuteAudio().catch((e) => { console.error(e) }) }} variant={audioMuted ? 'danger' : 'primary'}>
-                <Icon source={audioMuted ? IconTypes.IconMicrophoneOff : IconTypes.IconMicrophoneOn} />
-              </Button>
-            </Tooltip>
-            <Tooltip text='Change microphone' position='bottom'>
-              <Button modifier='square' onClick={() => { showSelectMicForm().catch((e) => { console.error(e) }) }} >
-                <Icon source={IconTypes.IconSettings} />
-              </Button>
-            </Tooltip>
-          </>
-        }
-        <Tooltip text='Leave interpretation' position='bottom'>
-          <Button modifier='square' onClick={() => { showDisconnectPrompt().catch((e) => { console.error(e) }) }} aria-label='Test'>
-            <Icon source={IconTypes.IconLeave} />
-          </Button>
-        </Tooltip>
+    <Box className='LanguagePanel'>
+      <BoxHeader className='Header'>
+        <button className="DraggableButton" ref={refDragButton}><Icon source={IconTypes.IconDraggable} /></button>
+        <span className='Title'>Interpretation</span>
+        <Icon source={IconTypes.IconMinus} />
+        <Icon source={IconTypes.IconClose} />
+      </BoxHeader>
+      <div className='Container'>
+        <div className='row'>
+          <Text className='LanguageText'>
+            {capitalizeFirstLetter(props.languageName)}
+          </Text>
+          <div className='Toolbar'>
+              {props.role === Role.Interpreter &&
+                <>
+                  <Tooltip text={audioMuted ? 'Unmute microphone' : 'Mute microphone'} position='bottom'>
+                    <Button modifier='square' onClick={() => { handleMuteAudio().catch((e) => { console.error(e) }) }} variant={audioMuted ? 'danger' : 'primary'}>
+                      <Icon source={audioMuted ? IconTypes.IconMicrophoneOff : IconTypes.IconMicrophoneOn} />
+                    </Button>
+                  </Tooltip>
+                  <Tooltip text='Change microphone' position='bottom'>
+                    <Button modifier='square' onClick={() => { showSelectMicForm().catch((e) => { console.error(e) }) }} >
+                      <Icon source={IconTypes.IconSettings} />
+                    </Button>
+                  </Tooltip>
+                </>
+              }
+              <Tooltip text='Leave interpretation' position='bottom'>
+                <Button modifier='square' onClick={() => { showDisconnectPrompt().catch((e) => { console.error(e) }) }} aria-label='Test'>
+                  <Icon source={IconTypes.IconLeave} />
+                </Button>
+              </Tooltip>
+          </div>
+        </div>
+        <span>Volume</span>
+        <RangeSlider className='VolumeSlider' min={0} max={100} step={1} selectedValue={volume} onChange={(event) => { setVolume(parseInt(event.target.value)) }}/>
+        <span>Main floor</span><span>Interpreter</span>
       </div>
     </Box>
   )
--- src/iframe.ts
+++ src/iframe.ts
@@ -1,18 +1,64 @@
+import iframeResizer from 'iframe-resizer'
+
 export const initializeIFrame = (): void => {
-  const url = document.location.href
-  const iframes = [...window.parent.document.getElementsByTagName('iframe')]
-  // Ignore the protocol when comparing
-  const pluginIFrame = iframes.find((iframe) => iframe.src.split('//')[1] === url.split('//')[1])
+  const pluginIFrame = getIFrame()
   if (pluginIFrame != null) {
     const style = pluginIFrame.style
-    style.position = 'relative'
+    style.position = 'absolute'
     style.display = 'block'
     style.margin = 'auto'
-    style.width = '400px'
-    style.height = '115px'
     style.border = '0'
+    style.top = '16px'
+    style.right = '0'
+    style.left = '0'
   }
   // Take out overflow from body
   const body = document.getElementsByTagName('body')[0]
   body.style.overflow = 'hidden'
+
+  iframeResizer.iframeResizer({ log: true, sizeWidth: true }, pluginIFrame as HTMLElement)
+}
+
+export const toggleIFramePointerEvents = (enable: boolean): void => {
+  const pluginIFrame = getIFrame()
+  if (pluginIFrame != null) {
+    const style = pluginIFrame.style
+    if (enable) {
+      style.pointerEvents = 'inherit'
+      parent.document.body.style.cursor = 'inherit'
+    } else {
+      style.pointerEvents = 'none'
+      parent.document.body.style.cursor = 'grabbing'
+    }
+  }
+}
+
+export const moveIFrame = (x: number, y: number): void => {
+  const pluginIFrame = getIFrame()
+
+  const xMargin = 23
+  const yMargin = 20
+
+  let newX = x - xMargin
+  let newY = y - yMargin
+
+  if (pluginIFrame != null) {
+    newX = Math.max(0, newX)
+    newY = Math.max(0, newY)
+    newX = Math.min(newX, parent.window.innerWidth - parseInt(pluginIFrame.style.width))
+    newY = Math.min(newY, parent.window.innerHeight - parseInt(pluginIFrame.style.height))
+    const style = pluginIFrame.style
+    style.position = 'absolute'
+    style.left = `${newX}px`
+    style.top = `${newY}px`
+    style.right = 'inherit'
+  }
+}
+
+const getIFrame = (): HTMLIFrameElement | undefined => {
+  const url = document.location.href
+  const iframes = [...window.parent.document.getElementsByTagName('iframe')]
+  // Ignore the protocol when comparing
+  const pluginIFrame = iframes.find((iframe) => iframe.src.split('//')[1] === url.split('//')[1])
+  return pluginIFrame
 }
